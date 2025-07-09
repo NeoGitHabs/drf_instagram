77ba1ac3e35231140b35b92b1e880b3aab9274e5
Admin <mrrneo@outlook.com>
2025-07-09 15:19:29 +0600

    ...

--- social_network/social_network/asgi.py
+++ social_network/social_network/asgi.py
@@ -2,7 +2,7 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from social_network.instagram.routing import websocket_urlpatterns
+from instagram.routing import websocket_urlpatterns
 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
